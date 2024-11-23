c9b07e54c409c2285b1e7f3ee08270a9ba83850a
Wiktoria <wiktoriagithub@gmail.com>
2024-11-23 16:56:57 +0100

    usunięcie niepotrzebnych plików

--- Assets/PlatformScoreTigger.cs
+++ Assets/PlatformScoreTigger.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
+using TMPro; // Biblioteka dla obs³ugi TextMeshPro
 
-public class PlatformScoreTigger : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public TextMeshProUGUI scoreText; // Tekst do wyœwietlania wyniku
+    private int score = 0;           // Wynik gracza
+
+    // Funkcja zwiêkszaj¹ca wynik o 1
+    public void AddPoint()
     {
-        
+        score++; // Zwiêkszenie wyniku
+        UpdateScoreText(); // Zaktualizowanie tekstu wyniku
     }
 
-    // Update is called once per frame
-    void Update()
+    // Funkcja aktualizuj¹ca wyœwietlany tekst wyniku
+    private void UpdateScoreText()
     {
-        
+        scoreText.text = "Score: " + score; // Wyœwietlenie wyniku na ekranie
     }
 }
