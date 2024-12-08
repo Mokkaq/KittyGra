05a6c9fd5c3b0409e199a0f5739d2a59b7d81169
Mokkaq <gbugajska051@gmail.com>
2024-12-08 20:59:44 +0100

    score i pauza dziala

--- Assets/Scripts/labirynt/PunktyManL.cs
+++ Assets/Scripts/labirynt/PunktyManL.cs
@@ -1,20 +1,20 @@
 using UnityEngine;
-using TMPro;
+using TMPro; // Obs³uga TextMeshPro
+using UnityEngine.SceneManagement; // Obs³uga resetowania sceny
 
 public class PunktyManL : MonoBehaviour
 {
-    public static PunktyManL Instance; // Singleton
+    public static PunktyManL Instance; // Singleton do ³atwego odwo³ywania siê do tego skryptu
 
     public int aktualnePunkty = 0; // Aktualny wynik
-
-    public TextMeshProUGUI punktyText; // Odwo³anie do TextMeshPro dla wyœwietlania punktów
+    public TextMeshProUGUI punktyText; // Obiekt TextMeshPro do wyœwietlania wyniku
 
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject); // Nie niszcz obiektu podczas zmiany sceny
+            // NIE u¿ywamy DontDestroyOnLoad, aby punkty resetowa³y siê po restarcie gry
         }
         else
         {
@@ -24,24 +24,32 @@ public class PunktyManL : MonoBehaviour
 
     private void Start()
     {
-        // Aktualizacja tekstu na start
+        ResetujPunkty(); // Ustaw punkty na 0 przy starcie gry
+    }
+
+    public void DodajPunkty(int ilePunktow)
+    {
+        aktualnePunkty += ilePunktow; // Dodaj punkty
+        AktualizujTekst(); // Zaktualizuj wyœwietlany tekst
+    }
+
+    public void ResetujPunkty()
+    {
+        aktualnePunkty = 0; // Ustaw punkty na 0
+        AktualizujTekst(); // Zaktualizuj wyœwietlany tekst
+    }
+
+    private void AktualizujTekst()
+    {
         if (punktyText != null)
         {
-            punktyText.text = "Punkty: " + aktualnePunkty;
+            punktyText.text = "Punkty: " + aktualnePunkty; // Zaktualizuj tekst w TextMeshPro
         }
     }
 
-    // Metoda do zwiêkszania wyniku
-    public void DodajPunkty(int ilePunktow)
+    public void ResetujGre()
     {
-        aktualnePunkty += ilePunktow;
-
-        // Aktualizacja wyœwietlanego wyniku
-        if (punktyText != null)
-        {
-            punktyText.text = "Punkty: " + aktualnePunkty;
-        }
-
-        Debug.Log("Aktualny wynik: " + aktualnePunkty); // Informacja o wyniku w konsoli
+        ResetujPunkty(); // Zresetuj punkty
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Prze³aduj aktualn¹ scenê
     }
 }
